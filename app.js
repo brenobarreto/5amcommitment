6b40f6cd1ef1a25b8aeaee4913671fa30ff4d1f0
brenobarreto <breno.oliveira@gmail.com>
2020-02-19 22:48:13 -0300

    Change all files by the ones that handle authentication using Okta

--- app.js
+++ app.js
@@ -1,21 +1,121 @@
-//Imports Express module
-const express = require('express');
+var express = require("express");
+var path = require("path");
+var favicon = require("static-favicon");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 
-//Import body-parser module
-const bodyParser = require('body-parser');
+const dashboardRouter = require("./routes/dashboard");
+const publicRouter = require("./routes/public");
+const usersRouter = require("./routes/users");
 
-//Initiates Express for this app
-const app = express();
-
-//Set body-parser to parse HTML data
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//Sets pug as our templating language
-app.set('view engine', 'pug');
-
-//Defines what is rendered in the root route
-app.get('/', (req, res) => {
-    res.render('index', {"user": "John"});
+var app = express();
+var oktaClient = new okta.Client({
+  orgUrl: "https://dev-917669.okta.com",
+  token: "007iN6koXGErqvLRrP6r-9jphHY_-6JEVP0W992klV"
+});
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-917669.okta.com/oauth2/default",
+  client_id: "0oa2ceo14JhX7Gx9v4x6",
+  client_secret: "cC0JiOM4F8zAngs0sODd3-3rUNRy8bE5KRrbWTzY",
+  redirect_uri: "http://localhost:3002/users/callback",
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
 });
 
-app.listen(3001, console.log('The server has started on port 3001'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(favicon());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "lsdbhvsdbvlsdbvlsdbvlslbvlshbvlsudvbbsudvblsdkvblsdkbv",
+    resave: true,
+    saveUninitialized: false
+  })
+);
+app.use(
+  session({
+    secret: "asdf;lkjh3lkjh235l23h5l235kjh",
+    resave: true,
+    saveUninitialized: false
+  })
+);
+app.use(oidc.router);
+
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  oktaClient
+    .getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
+
+app.use("/", publicRouter);
+app.use("/dashboard", loginRequired, dashboardRouter);
+app.use('/users', usersRouter);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
